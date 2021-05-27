38ae20298244c74f8c53f5ed97c06e987974fadd
nikheelpandey <nikhilpandey360@gmail.com>
2021-05-27 17:20:17 +0530

    initial training

--- model.py
+++ model.py
@@ -6,82 +6,237 @@ from math import pi, cos
 import torch
 import torchvision
 import torch.nn as nn
-from torch import allclose
-from datetime import datetime
-import torch.nn.functional as tf 
-import torchvision.transforms as T
-import torchvision.models as models
-from torch.utils.data import DataLoader
-from torch.testing import assert_allclose
-from torchvision import datasets, transforms
-from tqdm import tqdm
+import torch.nn.functional as F 
+from torchvision.models import resnet50
+
+def get_backbone(backbone, castrate=True):
+    if castrate:
+        backbone.output_dim = backbone.fc.in_features
+        backbone.fc = torch.nn.Identity()
+    return backbone
+
+
+
+# def negative_cosine_similarity(p,z):
+#     return - F.cosine_similarity(p, z.detach(), dim=-1).mean()
+
+
+
+# class MLP(nn.Module):
+#     def __init__(self, in_dim, hidden_dim=2048, out_dim=2048):
+#         super().__init__()
+        
+#         self.layer1 = nn.Sequential(
+#             nn.Linear(in_dim, hidden_dim),
+#             nn.BatchNorm1d(hidden_dim),
+#             nn.ReLU(inplace=True) )
+        
+#         self.layer2 = nn.Sequential(
+#             nn.Linear(hidden_dim, hidden_dim),
+#             nn.BatchNorm1d(hidden_dim),
+#             nn.ReLU(inplace=True) )
+        
+#         self.layer3 = nn.Sequential(
+#             nn.Linear(hidden_dim, out_dim),
+#             nn.BatchNorm1d(out_dim))
+    
+#     def forward(self,x):
+        
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         x = self.layer3(x)
+        
+#         return x
+
+# class PredictionMLP(nn.Module):
+#     def __init__(self, in_dim = 2048, hidden_dim=512, out_dim = 2048):
+#         super().__init__()
+        
+#         self.layer1 = nn.Sequential(
+#             nn.Linear(in_dim, hidden_dim),
+#             nn.BatchNorm1d(hidden_dim),
+#             nn.ReLU(inplace=True)
+#             )
+#         self.layer2 = nn.Linear(hidden_dim,out_dim)
+        
+#     def forward(self,x):
+#         x = self.layer1(x)
+#         x = self.layer2(x)
+#         return x
+    
+    
+# class SimSiam(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+        
+#         backbone = get_backbone(resnet50())
+
+#         mpl=MLP(backbone.output_dim)
+
+#         self.encoder = nn.Sequential(
+#             backbone,
+#             mpl
+#         )
+        
+#         self.predictor = PredictionMLP()
+    
+#     def forward(self, x1, x2):
+#         f,h = self.encoder, self.predictor
+#         z1, z2 = f(x1), f(x2)
+#         p1, p2 = h(z1), h(z2)
+        
+#         loss = negative_cosine_similarity(p1,z2)/2 + negative_cosine_similarity(p2,z1)/2
+        
+#         return loss
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
 from torchvision.models import resnet50
 
 
-def negative_cosine_similarity(p,z):
-    return - F.cosine_similarity(p, z.detach(), dim=-1).mean()
+def D(p, z, version='simplified'): # negative cosine similarity
+    if version == 'original':
+        z = z.detach() # stop gradient
+        p = F.normalize(p, dim=1) # l2-normalize 
+        z = F.normalize(z, dim=1) # l2-normalize 
+        return -(p*z).sum(dim=1).mean()
+
+    elif version == 'simplified':# same thing, much faster. Scroll down, speed test in __main__
+        return - F.cosine_similarity(p, z.detach(), dim=-1).mean()
+    else:
+        raise Exception
 
 
 
-class MLP(nn.Module):
+class projection_MLP(nn.Module):
     def __init__(self, in_dim, hidden_dim=2048, out_dim=2048):
         super().__init__()
-        
-        self.layer1 = nn.Sequential(
-            nn.Linear(in_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
-            nn.ReLU(inplace=True) )
-        
-        self.layer2 = nn.Sequential(
-            nn.Linear(hidden_dim, hidden_dim),
-            nn.BatchNorm1d(hidden_dim),
-            nn.ReLU(inplace=True) )
-        
-        self.layer3 = nn.Sequential(
-            nn.Linear(hidden_dim, out_dim),
-            nn.BatchNorm1d(out_dim))
-    
-    def forward(self,x):
-        
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        
-        return x
-
-class PredictionMLP(nn.Module):
-    def __init__(self, in_dim = 2048, hidden_dim=512, out_dim = 2048):
-        super().__init__()
-        
+        ''' page 3 baseline setting
+        Projection MLP. The projection MLP (in f) has BN ap-
+        plied to each fully-connected (fc) layer, including its out- 
+        put fc. Its output fc has no ReLU. The hidden fc is 2048-d. 
+        This MLP has 3 layers.
+        '''
         self.layer1 = nn.Sequential(
             nn.Linear(in_dim, hidden_dim),
             nn.BatchNorm1d(hidden_dim),
             nn.ReLU(inplace=True)
-            )
-        self.layer2 = nn.Linear(hidden_dim,out_dim)
-        
-    def forward(self,x):
+        )
+        self.layer2 = nn.Sequential(
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(inplace=True)
+        )
+        self.layer3 = nn.Sequential(
+            nn.Linear(hidden_dim, out_dim),
+            nn.BatchNorm1d(hidden_dim)
+        )
+        self.num_layers = 3
+    def set_layers(self, num_layers):
+        self.num_layers = num_layers
+
+    def forward(self, x):
+        if self.num_layers == 3:
+            x = self.layer1(x)
+            x = self.layer2(x)
+            x = self.layer3(x)
+        elif self.num_layers == 2:
+            x = self.layer1(x)
+            x = self.layer3(x)
+        else:
+            raise Exception
+        return x 
+
+
+class prediction_MLP(nn.Module):
+    def __init__(self, in_dim=2048, hidden_dim=512, out_dim=2048): # bottleneck structure
+        super().__init__()
+        ''' page 3 baseline setting
+        Prediction MLP. The prediction MLP (h) has BN applied 
+        to its hidden fc layers. Its output fc does not have BN
+        (ablation in Sec. 4.4) or ReLU. This MLP has 2 layers. 
+        The dimension of h’s input and output (z and p) is d = 2048, 
+        and h’s hidden layer’s dimension is 512, making h a 
+        bottleneck structure (ablation in supplement). 
+        '''
+        self.layer1 = nn.Sequential(
+            nn.Linear(in_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(inplace=True)
+        )
+        self.layer2 = nn.Linear(hidden_dim, out_dim)
+        """
+        Adding BN to the output of the prediction MLP h does not work
+        well (Table 3d). We find that this is not about collapsing. 
+        The training is unstable and the loss oscillates.
+        """
+
+    def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
-        return x
-    
-    
+        return x 
+
 class SimSiam(nn.Module):
     def __init__(self, backbone=resnet50()):
         super().__init__()
         
-        self.encoder = nn.Sequential(
-            backbone,
-            MLP
+        self.backbone = get_backbone(backbone)
+        self.projector = projection_MLP(backbone.output_dim)
+
+        self.encoder = nn.Sequential( # f encoder
+            self.backbone,
+            self.projector
         )
-        
-        self.predictor = PredictionMLP()
+        self.predictor = prediction_MLP()
     
     def forward(self, x1, x2):
-        f,h = self.encoder, self.predictor
+
+        f, h = self.encoder, self.predictor
         z1, z2 = f(x1), f(x2)
         p1, p2 = h(z1), h(z2)
-        
-        loss = negative_cosine_similarity(p1,z2)/2 + negative_cosine_similarity(p2,z1)/2
-        
-        return loss
+        L = D(p1, z2) / 2 + D(p2, z1) / 2
+        return L
+
+
+
+
+
+
+if __name__ == "__main__":
+    model = SimSiam()
+    x1 = torch.randn((2, 3, 224, 224))
+    x2 = torch.randn_like(x1)
+
+    model.forward(x1, x2).backward()
+    print("forward backwork check")
+
+    z1 = torch.randn((200, 2560))
+    z2 = torch.randn_like(z1)
+    import time
+    tic = time.time()
+    print(D(z1, z2, version='original'))
+    toc = time.time()
+    print(toc - tic)
+    tic = time.time()
+    print(D(z1, z2, version='simplified'))
+    toc = time.time()
+    print(toc - tic)
+
+# Output:
+# tensor(-0.0010)
+# 0.005159854888916016
+# tensor(-0.0010)
+# 0.0014872550964355469
+
+
+
+
+
+
+
+
+
+
+
+
